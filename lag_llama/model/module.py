8657c98797fdee03ab2b23e07a96aa3870aeb118
Kashif Rasul <kashif.rasul@gmail.com>
2024-02-09 14:12:44 +0100

    use F. scaled_dot_product_attention

--- lag_llama/model/module.py
+++ lag_llama/model/module.py
@@ -3,14 +3,13 @@ from dataclasses import dataclass
 from typing import List, Optional
 
 import torch
-from gluonts.torch.scaler import MeanScaler, NOPScaler, StdScaler
-from gluonts.torch.util import lagged_sequence_values, unsqueeze_expand
 from torch import nn
 from torch.nn import functional as F
+from gluonts.torch.distributions import DistributionOutput
+from gluonts.torch.scaler import MeanScaler, NOPScaler, StdScaler
+from gluonts.torch.util import lagged_sequence_values, unsqueeze_expand
 
 from gluon_utils.scalers.robust_scaler import RobustScaler
-from gluonts.torch.distributions import DistributionOutput
-
 
 
 @dataclass
@@ -22,32 +21,6 @@ class LTSMConfig:
     n_embd_per_head: int = 128
     rope_scaling: Optional[dict] = None
     dropout: float = 0.0
-
-
-def scaled_dot_product_attention(
-    query, key, value, attn_mask=None, dropout_p=0.0, is_causal=False, scale=None
-) -> torch.Tensor:
-    L, S = query.size(-2), key.size(-2)
-    scale_factor = 1 / math.sqrt(query.size(-1)) if scale is None else scale
-    attn_bias = torch.zeros(L, S, dtype=query.dtype, device=query.device)
-    if is_causal:
-        assert attn_mask is None
-        temp_mask = torch.ones(L, S, dtype=torch.bool, device=query.device).tril(
-            diagonal=0
-        )
-        attn_bias.masked_fill_(temp_mask.logical_not(), float("-inf"))
-        attn_bias.to(query.dtype)
-
-    if attn_mask is not None:
-        if attn_mask.dtype == torch.bool:
-            attn_mask.masked_fill_(attn_mask.logical_not(), float("-inf"))
-        else:
-            attn_bias += attn_mask
-    attn_weight = query @ key.transpose(-2, -1) * scale_factor
-    attn_weight += attn_bias
-    attn_weight = torch.softmax(attn_weight, dim=-1)
-    attn_weight = torch.dropout(attn_weight, dropout_p, train=True)
-    return attn_weight @ value
 
 
 class Block(nn.Module):
@@ -335,11 +308,11 @@ class CausalSelfAttention(nn.Module):
         # When using kv cache at inference, is_causal=False since decoder is causal, at each generation step we want
         # to avoid recalculating the same previous token attention
         if use_kv_cache:
-            y = scaled_dot_product_attention(
+            y = F.scaled_dot_product_attention(
                 q, k, v, attn_mask=None, dropout_p=self.dropout, is_causal=False
             )
         else:
-            y = scaled_dot_product_attention(
+            y = F.scaled_dot_product_attention(
                 q, k, v, attn_mask=None, dropout_p=self.dropout, is_causal=True
             )
 
